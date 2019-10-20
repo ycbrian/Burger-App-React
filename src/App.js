5fb68a8cf0f54d76ec38e98775037d43ad7e015a
Brian <brian19931009@gmail.com>
2019-10-21 03:01:06 +0800

    Logout UI and Logout component,link setting (Authentication)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/orders' component={Orders} />
           <Route path='/checkout' component={Checkout} />
           <Route path='/auth' component={Auth} />
+          <Route path='/logout' component={Logout} />
         </Switch>
       </Layout>
     </div>
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import Aux from '../Aux/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -20,8 +21,13 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-                <Toolbar DToggleClick={this.DrawerToggle}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClose}/>
+                <Toolbar 
+                    isAuth={this.props.isAuthenticated}
+                    DToggleClick={this.DrawerToggle}/>
+                <SideDrawer 
+                    isAuth={this.props.isAuthenticated}
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClose}/>
                 <main className={classes.Content}>
                     {this.props.children}
                 </main>
@@ -30,4 +36,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+}
+
+export default connect(mapStateToProps)(Layout);
