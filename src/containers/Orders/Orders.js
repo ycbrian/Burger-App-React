1aaf44bfd5a5169f60b42e79e4a7a02191ae7e59
Brian <brian19931009@gmail.com>
2019-10-23 14:26:08 +0800

    Guarding routes and fetching specific order with userId (Authentication)

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,7 +8,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
     componentDidMount() {
-        this.props.FetchOrders(this.props.token);
+        this.props.FetchOrders(this.props.token, this.props.userId);
     }
 
     render() {
@@ -33,13 +33,14 @@ const mapStateToProps = state => {
     return {
         orders: state.order.orders,
         loading: state.order.loading,
-        token: state.auth.token
+        token: state.auth.token,
+        userId: state.auth.userId
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        FetchOrders: (token) => dispatch(actions.fetchOrders(token))
+        FetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     };
 }
 
